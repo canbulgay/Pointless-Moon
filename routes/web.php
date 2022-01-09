c1f0487db91ed0749a6b0117f36ff0323cf3aaa0
canbulgay <canbulgay@outlook.com>
2022-01-09 10:46:27 +0300

    Add basic Excel read example

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use Shopify\Clients\Rest;
 use Google\Cloud\Translate\V2\TranslateClient;
+use Illuminate\Support\Facades\Storage;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,8 +32,28 @@ Route::get('shopifytest', function (Rest $client) {
 Route::get('translatetest', function (TranslateClient $translate) {
     
     $result = $translate->translate(
-        'Hello World!'
+        'Convert Daily Grasshoper'
     );
 
     return $result['text'];
+});
+
+/*
+! Boş bir php projesi açın ve composerla kurulumu yapın.
+? Örnekleri incele
+*/
+
+
+Route::get('spreadsheettest', function () {
+
+    $reader = IOFactory::createReaderForFile(Storage::path(''));
+    $reader->setReadDataOnly(true);
+    $spreadsheet = $reader->load(Storage::path(''));
+
+    $text = "";
+    for ($i=6; $i < 104 ; $i++) { 
+        $text .= $spreadsheet->getActiveSheet()->getCell('E' . $i). "<br>";
+    }
+    
+    return $text;
 });
