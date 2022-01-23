5415ae6ea235b44a0601658a0902413eb580255b
canbulgay <canbulgay@outlook.com>
2022-01-23 10:19:21 +0300

    Move CreateShopifyFromExcellCommand to CreateShopifyProductsFromExcelJob

--- app/Console/Commands/CreateShopifyProductsFromExcelCommand.php
+++ app/Console/Commands/CreateShopifyProductsFromExcelCommand.php
@@ -3,11 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Storage;
-use PhpOffice\PhpSpreadsheet\IOFactory;
-use App\Jobs\CreateProductsOnShopifyJob;
-use App\DTO\Product;
-use App\Helpers\TranslateHelper as TranslateClient;
+use App\Jobs\CreateShopifyProductsFromExcelJob;
 
 class CreateShopifyProductsFromExcelCommand extends Command
 {
@@ -40,54 +36,11 @@ class CreateShopifyProductsFromExcelCommand extends Command
      *
      * @return int
      */
-    public function handle(TranslateClient $translater)
+    public function handle()
     {
-        /*
-        ! Get Excell File
-        */
-        $spreadsheet = IOFactory::load(Storage::path('demo.xls'));
-        $productsData = Product::createCollectionFromExcel($spreadsheet);
+
+        CreateShopifyProductsFromExcelJob::dispatch();
         
-            foreach ($productsData as $productData) {
-
-                $productToCreate = [
-                    "title" =>
-                    $translater->translate($productData->type) . " " .
-                        $productData->collection . ", " .
-                        $translater->translate(
-                            $productData->color
-                        ) . ", "
-                        . $productData->sizeName,
-    
-                    "body_html" => "<strong>" . $translater->translate(
-                        $productData->type . " " . $productData->category
-                    ) . "!</strong>",
-    
-                    "vendor" => $productData->brand,
-    
-                    "product_type" => $translater->translate(
-                        $productData->type
-                    ),
-    
-                    "variants" => [
-                        [
-                            "sku" => $productData->code,
-                            "price" => $productData->price,
-                        ]
-                    ],
-    
-                    "images" => $productData->images,
-                ];
-
-            $this->line($productToCreate["title"] . " creating with " . count($productToCreate["images"]) . " images...");
-
-            $dispatchedJob = CreateProductsOnShopifyJob::dispatch(($productToCreate));
-
-            $this->info("Create product job has been dispatched.");
-            $this->line(" ");
-            
-            }
-
         $this->info("Finished");
 
         }
---
+++ app/Jobs/CreateShopifyProductsFromExcelJob.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use App\Jobs\CreateProductsOnShopifyJob;
+use App\DTO\Product;
+use App\Helpers\TranslateHelper as TranslateClient;
+
+class CreateShopifyProductsFromExcelJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle(TranslateClient $translater)
+    {
+        $spreadsheet = IOFactory::load(Storage::path('demo.xls'));
+        $productsData = Product::createCollectionFromExcel($spreadsheet);
+        
+            foreach ($productsData as $productData) {
+
+                $productToCreate = [
+                    "title" =>
+                    $translater->translate($productData->type) . " " .
+                        $productData->collection . ", " .
+                        $translater->translate(
+                            $productData->color
+                        ) . ", "
+                        . $productData->sizeName,
+    
+                    "body_html" => "<strong>" . $translater->translate(
+                        $productData->type . " " . $productData->category
+                    ) . "!</strong>",
+    
+                    "vendor" => $productData->brand,
+    
+                    "product_type" => $translater->translate(
+                        $productData->type
+                    ),
+    
+                    "variants" => [
+                        [
+                            "sku" => $productData->code,
+                            "price" => $productData->price,
+                        ]
+                    ],
+    
+                    "images" => $productData->images,
+                ];
+            
+                $dispatchedJob = CreateProductsOnShopifyJob::dispatch(($productToCreate));
+        
+            }
+    }
+}
